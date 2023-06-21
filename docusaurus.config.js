25de298fe985ac487b049edcdafe4b74e204d2cf
nysdas <nysdas@icloud.com>
2023-06-21 11:52:42 -0400

    fix starter code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/postgraphdb/postgraphdb.github.io/tree/code/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/postgraphdb/postgraphdb.github.io/tree/code/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -76,7 +76,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/PostGraphDB',
             label: 'GitHub',
@@ -101,7 +101,7 @@ const config = {
             items: [
               {
                 label: 'Discord',
-                href: 'https://postgraphdb.github.io/',
+                href: 'https://discord.gg/dhzFGvxGhn',
               },
               {
                 label: 'Twitter',
