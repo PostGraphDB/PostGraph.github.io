bb801a940783d4c3e1f1bd210bbcd3b362d9b5c0
nysdas <nysdas@icloud.com>
2023-06-21 12:08:32 -0400

    remove .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/arunesh/data/projects/postgraphdb.github.io/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/arunesh/data/projects/postgraphdb.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/arunesh/data/projects/postgraphdb.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/arunesh/data/projects/postgraphdb.github.io/src/css/custom.css'),
-];
